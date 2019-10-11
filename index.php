198910699ebda23e594fb5c50d794aad94ed3f2f
husnifathu <50961199+husnifathu@users.noreply.github.com>
2019-10-11 21:11:44 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,8 +1,7 @@
 <html>
 <body>
 <body bgcolor="blue">
-<table border="1" cellpadding="10">
-<center><h1>Nim : 10116278<h1></center>
-<p><center><h1>Nama : Husnie Fathu Rozaq<h1></center><p>
+<center><marquee><h1><font color="yellow">Nim : 10116278<h1></font></center></marquee>
+<p><center><marquee><h1><font color="yellow">Nama : Husnie Fathu Rozaq<h1></font></center></marquee></p>
 </body>          
 </html>
