6dfccf2f1b17913fa4677af4f2ed9c3c151d1efb
husnifathu <50961199+husnifathu@users.noreply.github.com>
2019-10-11 21:08:47 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,8 @@
+<html>
+<body>
+<body bgcolor="blue">
+<table border="1" cellpadding="10">
+<center><h1>Nim : 10116278<h1></center>
+<p><center><h1>Nama : Husnie Fathu Rozaq<h1></center><p>
+</body>          
+</html>
