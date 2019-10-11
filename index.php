2a66b3a5c99d88a3e1608649edbc333a64700e08
husnifathu <50961199+husnifathu@users.noreply.github.com>
2019-10-11 21:28:54 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,28 @@
-<html>
-<body>
-<body bgcolor="blue">
-<center><marquee><h1><font color="yellow">Nim : 10116278<h1></font></center></marquee>
-<p><center><marquee><h1><font color="yellow">Nama : Husnie Fathu Rozaq<h1></font></center></marquee></p>
-</body>          
-</html>
+<style>
+	div.a {
+	position: absolute;
+	margin-top: -100px;
+	margin-left: -200px;
+	left: 50%;
+	top: 50%;
+	width: 400px;
+	height: 220px;
+}
+</style>
+
+<div class ="a">
+<?php
+	echo "<table border = '2' >";
+	echo "<tr>";
+	echo "<td> NIM </td>" ;
+	echo "<td> 10116278 </td> " ;
+	echo "</tr>";
+	echo "<tr>";
+	echo "<td> Nama </td>"; 
+	echo "<td> Husnie Fathu Rozaq </td> " ;
+	echo "</tr>";
+	echo "</table>";
+?>
+
+</div>
+	
